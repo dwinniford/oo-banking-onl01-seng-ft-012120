3d1b6ee1eb58f5ccd05a690dbde815023039792f
Dave Winniford <dwinniford@gmail.com>
2020-01-31 17:15:28 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+  attr_accessor :name
+  attr_reader :balance, :status 
+  
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(amount)
+    @balance += amount 
+  end 
+  
+  def display_balance
+    @balance 
+  end 
+  
+  def valid?
+    if @status == "open" && @balance > 0 
+      true 
+    else 
+      false 
+    end 
+  end 
+  
+  def close_account
+    @status = "closed"
+    @balance = 0 
+  end 
   
 end
